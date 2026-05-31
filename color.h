7ab80efa2ae003088e4161177d7ede8bf5547ae4
Yunho Cho <ycho358@gatech.edu>
2026-05-31 16:06:17 -0700

    Chapter 10.5

--- color.h
+++ color.h
@@ -29,7 +29,7 @@ void write_color(std::ostream& out, const color& pixel_color) {
   static const interval intensity(0.000, 0.999);
   int rbyte = int(256 * intensity.clamp(r));
   int gbyte = int(256 * intensity.clamp(g));
-  int bbyte = int(256 * b);
+  int bbyte = int(256 * intensity.clamp(b));
 
   // Write out the pixel color components.
   out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
--- sphere.h
+++ sphere.h
@@ -6,7 +6,8 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
       vec3 oc = center - r.origin();
@@ -32,6 +33,7 @@ class sphere : public hittable {
       rec.p = r.at(rec.t);
       vec3 outward_normal = (rec.p - center) / radius;
       rec.set_face_normal(r, outward_normal);
+      rec.mat = mat;
 
       return true;
     }
@@ -39,6 +41,7 @@ class sphere : public hittable {
     private:
       point3 center;
       double radius;
+      shared_ptr<material> mat;
 };
 
 #endif
